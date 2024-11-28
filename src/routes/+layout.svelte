5433cdd1d73eb4638326a158242712cb753d199f
Jakub Hanslík <jakub@hanslikovi.com>
2024-11-28 13:18:19 +0100

    4 sidebar (#8)
    * Shadcn navbar setup
    
    * Sidebar template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  	import AppSidebar from "$lib/components/app-sidebar.svelte";
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-	/>
-</svelte:head>
+<Sidebar.Provider>
+	<AppSidebar />
+	<main>
+	  <Sidebar.Trigger />
+	  {@render children?.()}
+	</main>
+</Sidebar.Provider>
 
 {@render children()}

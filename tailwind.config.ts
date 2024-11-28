44e65a18491c471ffe0e38652cf6809c67bcfce8
Kuba Vejražka <jakvejr@gmail.com>
2024-11-28 11:50:00 +0100

    barvicky

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,64 +1,85 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
-import type { Config } from "tailwindcss";
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	safelist: ["dark"],
-	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px"
-			}
-		},
-		extend: {
-			colors: {
-				border: "hsl(var(--border) / <alpha-value>)",
-				input: "hsl(var(--input) / <alpha-value>)",
-				ring: "hsl(var(--ring) / <alpha-value>)",
-				background: "hsl(var(--background) / <alpha-value>)",
-				foreground: "hsl(var(--foreground) / <alpha-value>)",
-				primary: {
-					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
-					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
-				},
-				card: {
-					DEFAULT: "hsl(var(--card) / <alpha-value>)",
-					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
-				}
-			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)"
-			},
-			fontFamily: {
-				sans: [...fontFamily.sans]
-			}
-		}
-	},
+  darkMode: ['class'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        text: {
+          DEFAULT: '#03040a',
+          50: '#060814',
+          100: '#0c1027',
+          200: '#17204f',
+          300: '#233076',
+          400: '#2f3f9d',
+          500: '#3b4fc4',
+          600: '#6272d0',
+          700: '#8996dc',
+          800: '#b0b9e8',
+          900: '#d8dcf3',
+          950: '#ebedf9'
+        },
+        background: {
+          DEFAULT: '#f4f5fc',
+          50: '#050714',
+          100: '#0b0e28',
+          200: '#161d50',
+          300: '#212b78',
+          400: '#2c39a0',
+          500: '#3748c8',
+          600: '#5f6cd3',
+          700: '#8791de',
+          800: '#afb6e9',
+          900: '#d7daf4',
+          950: '#ebedfa'
+        },
+        primary: {
+          DEFAULT: '#435fcb',
+          50: '#050814',
+          100: '#0b1128',
+          200: '#162250',
+          300: '#213278',
+          400: '#2c43a0',
+          500: '#3754c8',
+          600: '#5f76d3',
+          700: '#8798de',
+          800: '#afbbe9',
+          900: '#d7ddf4',
+          950: '#ebeefa'
+        },
+        secondary: {
+          DEFAULT: '#ce8bdf',
+          50: '#110514',
+          100: '#220b28',
+          200: '#441650',
+          300: '#672178',
+          400: '#892ca0',
+          500: '#ab37c8',
+          600: '#bc5fd3',
+          700: '#cd87de',
+          800: '#ddafe9',
+          900: '#eed7f4',
+          950: '#f7ebfa'
+        },
+        accent: {
+          DEFAULT: '#d873d0',
+          50: '#140613',
+          100: '#280b25',
+          200: '#50164b',
+          300: '#772270',
+          400: '#9f2d96',
+          500: '#c738bb',
+          600: '#d260c9',
+          700: '#dd88d6',
+          800: '#e9afe4',
+          900: '#f4d7f1',
+          950: '#f9ebf8'
+        }
+      }
+    }
+  }
 };
 
 export default config;

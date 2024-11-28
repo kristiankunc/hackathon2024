a50aa771bad7df98504acf284fd73541105c41c9
Kristián Kunc <kristian.kunc@gmail.com>
2024-11-28 19:15:32 +0100

    put user in if statement

--- src/routes/employees/[id]/+page.svelte
+++ src/routes/employees/[id]/+page.svelte
@@ -6,24 +6,24 @@
 
 {#if !$page.data.employee}
 	<h1>Employee not found</h1>
+{:else}
+	<div>
+		<p>User data</p>
+		<h1>{$page.data.employee.firstName} {$page.data.employee.lastName}</h1>
+		<p>{$page.data.employee.email}</p>
+		<p>{$page.data.employee.category.name} ({$page.data.employee.category.description})</p>
+	</div>
+
+	<div>
+		<p>Tests participated</p>
+		{#if !$page.data.employee.tests}
+			<p>No tests participated</p>
+		{/if}
+		{#each $page.data.employee.tests as test}
+			<div>
+				<p>{test.name}</p>
+				<p>{test.description}</p>
+			</div>
+		{/each}
+	</div>
 {/if}
-
-<div>
-	<p>User data</p>
-	<h1>{$page.data.employee.firstName} {$page.data.employee.lastName}</h1>
-	<p>{$page.data.employee.email}</p>
-	<p>{$page.data.employee.category.name} ({$page.data.employee.category.description})</p>
-</div>
-
-<div>
-	<p>Tests participated</p>
-	{#if !$page.data.employee.tests.length}
-		<p>No tests participated</p>
-	{/if}
-	{#each $page.data.employee.tests as test}
-		<div>
-			<p>{test.name}</p>
-			<p>{test.description}</p>
-		</div>
-	{/each}
-</div>

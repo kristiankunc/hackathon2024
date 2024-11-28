39159b63a60929790f0ccf8ca87778a093dce485
Kristián Kunc <kristian.kunc@gmail.com>
2024-11-28 15:41:38 +0100

    add import

---
+++ prisma/seed.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const main = async () => {
+	await prisma.employeeCategory.createMany({
+		data: [
+			{
+				name: 'IT',
+				description: 'IT Department'
+			},
+			{
+				name: 'HR',
+				description: 'HR Department'
+			},
+			{
+				name: 'Finance',
+				description: 'Finance Department'
+			},
+			{
+				name: 'Marketing',
+				description: 'Marketing Department'
+			},
+			{
+				name: 'Sales',
+				description: 'Sales Department'
+			}
+		]
+	});
+
+	await prisma.employee.createMany({
+		data: [
+			{
+				firstName: 'John',
+				lastName: 'Doe',
+				email: 'john.doe@example.com',
+				employeeCategoryId: 1
+			},
+			{
+				firstName: 'Jane',
+				lastName: 'Doe',
+				email: 'jane.doe@example.com',
+				employeeCategoryId: 2
+			},
+			{
+				firstName: 'Alice',
+				lastName: 'Smith',
+				email: 'alice.smith@example.com',
+				employeeCategoryId: 3
+			},
+			{
+				firstName: 'Bob',
+				lastName: 'Brown',
+				email: 'bob.brown@example.com',
+				employeeCategoryId: 4
+			},
+
+			{
+				firstName: 'Charlie',
+				lastName: 'White',
+				email: 'charlie.white@example.com',
+				employeeCategoryId: 5
+			}
+		]
+	});
+};
+
+main()
+	.catch((e) => {
+		throw e;
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
---
+++ src/routes/employees/import/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { page } from '$app/stores';
+</script>
+
+<form method="POST" action="?/importFle" enctype="multipart/form-data">
+	<p>File</p>
+	<Input accept=".csv" type="file" name="file" required />
+	<Button type="submit">Submit</Button>
+</form>

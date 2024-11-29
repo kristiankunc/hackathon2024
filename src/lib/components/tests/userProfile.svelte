cc08571794e381b229b9089a013c73f1df2f9dd3
Kuba Vejražka <jakvejr@gmail.com>
2024-11-29 03:57:44 +0100

    results page now shows who clicked the link

--- src/lib/components/tests/userProfile.svelte
+++ src/lib/components/tests/userProfile.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
-	const mail = 'test@example.com';
-	const name = 'Šimon Bendl';
-	let { employee, passed = false } = $props();
+	let { log } = $props();
 
-	import CircleUser from 'lucide-svelte/icons/circle-user';
-	import Ellipsis from 'lucide-svelte/icons/ellipsis';
-	import Check from 'lucide-svelte/icons/check';
-	import Ban from 'lucide-svelte/icons/ban';
 	import Button from '../ui/button.svelte';
-	// import Button from '../ui/button/button.svelte';
+
 </script>
 
-<div class="bg-background mb-2 flex w-full items-center justify-between rounded-lg p-4">
-	<div class="flex items-center gap-4">
-		<CircleUser />
-		<h3 class="font-semibold">{employee.firstName} {employee.lastName}</h3>
-		<h4 class="text-sm">{employee.email}</h4>
+<div class="mb-2 flex w-full items-center justify-between rounded-lg bg-background p-4">
+	<div class="flex items-center gap-2">
+    <span class="material-symbols-outlined">person</span>
+		<h3 class="font-semibold">{log.employee.firstName} {log.employee.lastName}</h3>
+		<h4 class="text-xs">{log.employee.email}</h4>
 	</div>
-  <div class="flex items-center gap-8">
-	{#if true}
-		<Check class="text-[#51c6aa]" />
-	{:else}
-		<Ban class="text-[#cf7d6d]" />
-	{/if}
-	<Button preset="skeleton">
-		<Ellipsis />
-	</Button>
-</div>
+	<div class="flex items-center gap-8">
+		{#if log.latestLog.action == 'READ'}
+			<span class="material-symbols-outlined text-[#51c6aa]">check</span>
+		{:else if log.latestLog.action == 'CLICKED'}
+			<span class="material-symbols-outlined text-[#cf7d6d]">warning</span>
+		{:else}
+			<span class="material-symbols-outlined text-primary">visibility_off</span>
+		{/if}
+		<Button preset="borderless" colorScheme="text">
+      <span class="material-symbols-outlined">more_vert</span>
+		</Button>
+	</div>
 </div>

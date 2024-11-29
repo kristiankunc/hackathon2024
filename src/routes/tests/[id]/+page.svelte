ea4d475a9aa870abb0982224692f9206c962658a
Kristián Kunc <kristian.kunc@gmail.com>
2024-11-29 03:33:54 +0100

    get latest logs in tests page

--- src/routes/tests/[id]/+page.svelte
+++ src/routes/tests/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import UserProfile from '$lib/components/tests/userProfile.svelte';
 	import { BarChart, Chart, ColorRamp, Pie, PieChart, Svg } from 'layerchart';
 
